7c1c02b0711b46ab2a18094d8b0eac4f5fb89739
T Sathwik <your_email@gmail.com>
2026-07-02 11:20:44 +0530

    Added weather API integration

---
+++ Projects/01-api-basics/weather_api/weather.py
@@ -0,0 +1,32 @@
+import requests
+from config import API_KEY, BASE_URL
+
+
+def welcome():
+    print("=" * 40)
+    print("      Weather API Explorer")
+    print("=" * 40)
+
+
+def get_city():
+    while True:
+        city = input("Enter city name: ").strip()
+
+        if city == "":
+            print("❌ City name cannot be empty.\n")
+        elif not city.replace(" ", "").isalpha():
+            print("❌ Invalid city name.\n")
+        else:
+            return city
+
+
+def get_weather(city):
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+
+    response = requests.get(BASE_URL, params=params)
+
+    return response.json()
